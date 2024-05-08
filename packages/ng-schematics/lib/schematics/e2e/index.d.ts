67c16c09614615dddfabd1bc357e328e86d22279
Marcelo Machado Fleury <marcelomf@gmail.com>
2024-05-08 15:31:11 -0300

    remove itens

--- packages/ng-schematics/lib/schematics/e2e/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright 2023 Google Inc.
- * SPDX-License-Identifier: Apache-2.0
- */
-import { type Rule } from '@angular-devkit/schematics';
-export declare function e2e(userArgs: Record<string, string>): Rule;
-//# sourceMappingURL=index.d.ts.map
